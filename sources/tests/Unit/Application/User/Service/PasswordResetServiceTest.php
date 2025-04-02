36f5be9e605b0755b94260b2ca17c359c3da30ea
ZemEr1001 <egor.zemtzovskij@yandex.ru>
2025-04-02 16:39:59 +0300

    refactor PasswordResetServiceTest

--- sources/tests/Unit/Application/User/Service/PasswordResetServiceTest.php
+++ sources/tests/Unit/Application/User/Service/PasswordResetServiceTest.php
@@ -14,104 +14,121 @@ use App\Domain\User\Event\PasswordChangeRequestedEvent;
 use App\Domain\User\Event\PasswordResetRequestedEvent;
 use App\Domain\User\Repository\PasswordResetTokenRepository;
 use App\Domain\User\Repository\UserRepositoryInterface;
+use App\Domain\User\Service\PasswordHasher;
 use App\Domain\User\ValueObject\ResetPasswordToken;
 use DateInterval;
 use DateTimeImmutable;
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use PHPUnit\Framework\TestCase;
 
-final class PasswordResetServiceTest extends KernelTestCase
+final class PasswordResetServiceTest extends TestCase
 {
+    private MailHandlerInterface $mailHandlerStub;
+    private UserRepositoryInterface $userRepositoryStub;
+    private PasswordResetTokenRepository $tokenRepositoryStub;
+    private PasswordHasher $passwordHasherStub;
+    private PasswordResetService $sut;
+
+    public function setUp(): void
+    {
+        $this->mailHandlerStub = $this->createStub(MailHandlerInterface::class);
+        $this->userRepositoryStub = $this->createStub(UserRepositoryInterface::class);
+        $this->passwordHasherStub = $this->createStub(PasswordHasher::class);
+        $this->tokenRepositoryStub = $this->createStub(PasswordResetTokenRepository::class);
+        $this->sut = new PasswordResetService(
+            userRepository: $this->userRepositoryStub,
+            tokenRepository: $this->tokenRepositoryStub,
+            passwordHasher: $this->passwordHasherStub,
+            mailHandler: $this->mailHandlerStub,
+        );
+    }
+
     public function testForgotPassword(): void
     {
-        $userRepository = $this->createStub(UserRepositoryInterface::class);
-        $userRepository->method('findOneByEmail')->willReturn(new User(
-            id: 1, login: 'test', password: 'test', email: 'test@test.com',
-        ));
-        self::getContainer()->set(UserRepositoryInterface::class, $userRepository);
-        self::getContainer()->set(MailHandlerInterface::class, $this->createStub(MailHandlerInterface::class));
-        $sut = self::getContainer()->get(PasswordResetService::class);
+        $this->userRepositoryStub->method('findOneByEmail')->willReturn(
+            new User(
+                id: 1,
+                login: 'test',
+                password: 'test',
+                email: 'test@test.com',
+            )
+        );
 
-        $result = $sut->forgotPassword(new PasswordResetRequestDto(email: 'test@test.com',signature: ''));
+        $result = $this->sut->forgotPassword(new PasswordResetRequestDto(email: 'test@test.com', signature: ''));
 
         self::assertInstanceOf(expected: PasswordResetRequestedEvent::class, actual: $result);
     }
 
     public function testForgotPasswordNonexistentUser(): void
     {
-        $userRepository = $this->createStub(UserRepositoryInterface::class);
-        $userRepository->method('findOneByEmail')->willReturn(null);
-        self::getContainer()->set(UserRepositoryInterface::class, $userRepository);
-        $sut = self::getContainer()->get(PasswordResetService::class);
+        $this->userRepositoryStub->method('findOneByEmail')->willReturn(null);
 
-        $result = $sut->forgotPassword(new PasswordResetRequestDto(email: '',signature: ''));
+        $result = $this->sut->forgotPassword(new PasswordResetRequestDto(email: '', signature: ''));
 
         self::assertNull($result);
     }
 
     public function testChangePassword(): void
     {
-        $userRepository = $this->createStub(UserRepositoryInterface::class);
-        $userRepository->method('findOneByEmail')->willReturn(new User(
-            id: 1, login: 'test', password: 'test', email: 'test@test.com',
-        ));
-        self::getContainer()->set(UserRepositoryInterface::class, $userRepository);
-        $sut = self::getContainer()->get(PasswordResetService::class);
+        $this->userRepositoryStub->method('findOneByEmail')->willReturn(
+            new User(
+                id: 1,
+                login: 'test',
+                password: 'test',
+                email: 'test@test.com',
+            )
+        );
 
-        $result = $sut->changePassword(new PasswordChangeRequestDto(email: 'test@test.com', password: 'test'));
+        $result = $this->sut->changePassword(new PasswordChangeRequestDto(email: 'test@test.com', password: 'test'));
 
         self::assertInstanceOf(expected: PasswordChangeRequestedEvent::class, actual: $result);
     }
 
     public function testValidateNonexistentToken(): void
     {
-        $tokenRepository = $this->createStub(PasswordResetTokenRepository::class);
-        $tokenRepository->method('findTokenByEmail')->willReturn(null);
-        self::getContainer()->set(PasswordResetTokenRepository::class, $tokenRepository);
-        $sut = self::getContainer()->get(PasswordResetService::class);
+        $this->tokenRepositoryStub->method('findTokenByEmail')->willReturn(null);
 
-        $result = $sut->validateToken(new PasswordResetTokenDto(email: '',token: ''));
+        $result = $this->sut->validateToken(new PasswordResetTokenDto(email: '', token: ''));
 
         self::assertFalse($result);
     }
 
     public function testValidateExpiredToken(): void
     {
-        $tokenRepository = $this->createStub(PasswordResetTokenRepository::class);
-        $tokenRepository->method('findTokenByEmail')->willReturn(
-            new ResetPasswordToken(value: 'qwerty1234', expiresAt: (new DateTimeImmutable())->sub(new DateInterval('PT1H')),)
+        $this->tokenRepositoryStub->method('findTokenByEmail')->willReturn(
+            new ResetPasswordToken(
+                value: 'qwerty1234',
+                expiresAt: (new DateTimeImmutable())->sub(new DateInterval('PT1H'))
+            )
         );
-        self::getContainer()->set(PasswordResetTokenRepository::class, $tokenRepository);
-        $sut = self::getContainer()->get(PasswordResetService::class);
-
-        $result = $sut->validateToken(new PasswordResetTokenDto(email:'user@example.com', token: 'qwerty'));
+        
+        $result = $this->sut->validateToken(new PasswordResetTokenDto(email: 'user@example.com', token: 'qwerty'));
 
         self::assertFalse($result);
     }
 
     public function testValidateCorrectToken(): void
     {
-        $tokenRepository = $this->createStub(PasswordResetTokenRepository::class);
-        $tokenRepository->method('findTokenByEmail')->willReturn(
-            new ResetPasswordToken(value: 'qwerty1234', expiresAt: (new DateTimeImmutable())->add(new DateInterval('P1D')),)
+        $this->tokenRepositoryStub->method('findTokenByEmail')->willReturn(
+            new ResetPasswordToken(
+                value: 'qwerty1234',
+                expiresAt: (new DateTimeImmutable())->add(new DateInterval('P1D'))
+            )
         );
-        self::getContainer()->set(PasswordResetTokenRepository::class, $tokenRepository);
-        $sut = self::getContainer()->get(PasswordResetService::class);
-
-        $result = $sut->validateToken(new PasswordResetTokenDto(email:'user@example.com', token: 'qwerty1234'));
+        $result = $this->sut->validateToken(new PasswordResetTokenDto(email: 'user@example.com', token: 'qwerty1234'));
 
         self::assertTrue($result);
     }
 
     public function testValidateMismatchToken(): void
     {
-        $tokenRepository = $this->createStub(PasswordResetTokenRepository::class);
-        $tokenRepository->method('findTokenByEmail')->willReturn(
-            new ResetPasswordToken(value: 'qwerty1234', expiresAt: (new DateTimeImmutable())->add(new DateInterval('P1D')),)
+        $this->tokenRepositoryStub->method('findTokenByEmail')->willReturn(
+            new ResetPasswordToken(
+                value: 'qwerty1234',
+                expiresAt: (new DateTimeImmutable())->add(new DateInterval('P1D'))
+            )
         );
-        self::getContainer()->set(PasswordResetTokenRepository::class, $tokenRepository);
-        $sut = self::getContainer()->get(PasswordResetService::class);
 
-        $result = $sut->validateToken(new PasswordResetTokenDto(email:'user@example.com', token: 'asdf'));
+        $result = $this->sut->validateToken(new PasswordResetTokenDto(email: 'user@example.com', token: 'asdf'));
 
         self::assertFalse($result);
     }
